e3682464ccd697ecb344486cc675cb89cc299251
xzy <xzy@xzy-PC.localdomain>
2018-01-29 17:32:44 +0800

    spring boot 某一个服务的service demo

---
+++ src/main/java/com/gq/springboot/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.gq.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
