2a6edd4dc617daef1d681fc2bb1afd149ddbc20e
xzy <xzy@xzy-PC.localdomain>
2018-01-30 13:12:01 +0800

    增加了上传下载功能

--- src/main/java/com/gq/springboot/DemoApplication.java
+++ src/main/java/com/gq/springboot/DemoApplication.java
@@ -1,16 +1,165 @@
 package com.gq.springboot;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
+@Configuration
 public class DemoApplication {
+	private static Logger logger = Logger.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("service已开启...");
+	}
+
+	@RequestMapping(value = "/", produces = "text/plain;charset=UTF-8")
+	String index() {
+		return "Hello Spring Boot!";
+	}
+
+	/**
+	 * 单个文件上传
+	 */
+	@RequestMapping("fileUpload")
+	@ResponseBody
+	public String fileUpload(@RequestParam("fileName") MultipartFile file) {
+		if (file.isEmpty()) {
+			return "false";
+		}
+		String fileName = file.getOriginalFilename();
+		int size = (int) file.getSize();
+		System.out.println(fileName + "-->" + size);
+
+		String path = "c:/test";
+		File dest = new File(path + "/" + fileName);
+		if (!dest.getParentFile().exists()) { // 判断文件父目录是否存在
+			dest.getParentFile().mkdir();
+		}
+		try {
+			file.transferTo(dest); // 保存文件
+			return "true";
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "false";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "false";
+		}
+	}
+
+	/**
+	 * 多个文件上传
+	 */
+	@RequestMapping(value = "multifileUpload", method = RequestMethod.POST)
+	public @ResponseBody String multifileUpload(HttpServletRequest request) {
+		List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("fileName");
+		if (files.isEmpty()) {
+			return "false";
+		}
+
+		String path = "c:/mutipleTest";
+
+		for (MultipartFile file : files) {
+			String fileName = file.getOriginalFilename();
+			int size = (int) file.getSize();
+			System.out.println(fileName + "-->" + size);
+
+			if (file.isEmpty()) {
+				return "false";
+			} else {
+				File dest = new File(path + "/" + fileName);
+				if (!dest.getParentFile().exists()) { // 判断文件父目录是否存在
+					dest.getParentFile().mkdir();
+				}
+				try {
+					file.transferTo(dest);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return "false";
+				}
+			}
+		}
+		return "true";
+	}
+
+	@RequestMapping("download")
+	public String downLoad(HttpServletResponse response) {
+		String filename = "重构-改善既有代码的设计(简体中文).pdf";
+		String filePath = "c:/test";
+		File file = new File(filePath + "/" + filename);
+		if (file.exists()) { // 判断文件父目录是否存在
+			response.setContentType("application/force-download");
+			response.setHeader("Content-Disposition", "attachment;fileName=" + filename);
+
+			byte[] buffer = new byte[1024];
+			FileInputStream fis = null; // 文件输入流
+			BufferedInputStream bis = null;
+
+			OutputStream os = null; // 输出流
+			try {
+				os = response.getOutputStream();
+				fis = new FileInputStream(file);
+				bis = new BufferedInputStream(fis);
+				int i = bis.read(buffer);
+				while (i != -1) {
+					os.write(buffer);
+					i = bis.read(buffer);
+				}
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("----------file download" + filename);
+			try {
+				bis.close();
+				fis.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		// 单个文件最大
+		factory.setMaxFileSize("10240KB"); // KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("102400KB");
+		return factory.createMultipartConfig();
 	}
 }
